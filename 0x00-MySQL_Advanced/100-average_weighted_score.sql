6bff5571051b6fd8f0a3bcb2258266d2c94422af
EljonesA <122857130+EljonesA@users.noreply.github.com>
2024-07-13 11:24:46 +0300

    Create 100-average_weighted_score.sql

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,31 @@
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE total_weighted_score FLOAT;
+    DECLARE total_weight INT;
+    
+    -- Calculate the total weighted score
+    SELECT SUM(c.score * p.weight)
+    INTO total_weighted_score
+    FROM corrections c
+    JOIN projects p ON c.project_id = p.id
+    WHERE c.user_id = user_id;
+    
+    -- Calculate the total weight
+    SELECT SUM(p.weight)
+    INTO total_weight
+    FROM corrections c
+    JOIN projects p ON c.project_id = p.id
+    WHERE c.user_id = user_id;
+    
+    -- Update the average_score for the user
+    UPDATE users
+    SET average_score = (CASE 
+                            WHEN total_weight = 0 THEN 0 
+                            ELSE total_weighted_score / total_weight 
+                         END)
+    WHERE id = user_id;
+END //
+
+DELIMITER ;
