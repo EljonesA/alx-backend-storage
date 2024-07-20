e51ed4e7a4519a575fe2a683c78e4e18d0c2dea6
Eljones <eljones.odongo@gmail.com>
2024-07-20 07:26:21 +0300

    Reading from Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Redis -> writing strings to redis
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -25,6 +25,31 @@ class Cache:
         key = str(uuid.uuid4())  # generate random key
         self._redis.set(key, data)  # store input data in redis
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float, None]:
+        '''
+        Retrieve data from Redis & optionally convert it using given callable.
+        '''
+        data = self._redis.get(key)
+        if data is None:  # conserve redis.get behavior if key not present
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        ''' Retrieve a string from Redis. '''
+        data = self.get(key)
+        if data is None or not isinstance(data, bytes):
+            return None
+        return data.decode('utf-8')
+
+    def get_int(self, key: str) -> Optional[int]:
+        ''' Retrieve an integer from Redis. '''
+        data = self.get(key)
+        if data is None:
+            return None
+        return int(data)
 
 
 if __name__ == '__main__':
