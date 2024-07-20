c58b5f5950e7664304650cb62aea1321f5655184
Eljones <eljones.odongo@gmail.com>
2024-07-20 16:01:10 +0300

    Top students

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,28 @@
+#!/usr/bin/ env python3
+""" Python function that returns all students sorted by average score """
+
+from pymongo import MongoClient
+
+def top_students(mongo_collection):
+    ''' Returns a list of students sorted by average score '''
+    # Aggregate the students with their average scores
+    pipeline = [
+            {
+                "$project": {
+                    "name": 1,
+                    "averageScore": {
+                        "$avg": "$topics.score"
+                    }
+                }
+            },
+            {
+                "$sort": {
+                    "averageScore": -1
+                }
+            }
+    ]
+
+    # Execute the aggregation pipeline
+    students = list(mongo_collection.aggregate(pipeline))
+
+    return students
